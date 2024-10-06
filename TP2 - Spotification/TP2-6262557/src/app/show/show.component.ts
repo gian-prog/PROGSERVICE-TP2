f2f7d357972bc2b87efc5b60d2551bda4ab4ccf4
gian-prog <6262557@cegepmontpetit.ca>
2024-10-05 22:17:26 -0400

    FCT: Fin TP2 et google maps pour concerts

--- TP2 - Spotification/TP2-6262557/src/app/show/show.component.ts
+++ TP2 - Spotification/TP2-6262557/src/app/show/show.component.ts
@@ -8,11 +8,12 @@ import { Artist } from '../models/artist';
 import { CommonModule } from '@angular/common';
 import { BandsInTownService } from '../services/bandsInTownService'
 import { Show } from '../models/show';
+import { GoogleMapsModule } from '@angular/google-maps';
 
 @Component({
   selector: 'app-show',
   standalone: true,
-  imports: [FormsModule, RouterModule, RouterOutlet, TranslateModule, CommonModule],
+  imports: [FormsModule, RouterModule, RouterOutlet, TranslateModule, CommonModule, GoogleMapsModule],
   templateUrl: './show.component.html',
   styleUrl: './show.component.css'
 })
@@ -24,6 +25,11 @@ export class ShowComponent {
   shows: Show[] = []
 
   language: string = "fr"
+
+
+  center: { lat: number; lng: number } = { lat: 0, lng: 0 };
+  zoom: number = 2;
+
 
   constructor(public translate: TranslateService, public route: ActivatedRoute, public http: HttpClient, private bandsInTownService: BandsInTownService) {
     this.translate.setDefaultLang(this.language)
@@ -43,10 +49,37 @@ export class ShowComponent {
       if (this.shows.length === 0) {
         console.log('No events found for this artist.');
       }
+      else{        
+        this.calculerCentre(this.shows);}
     } catch (error) {
       console.error('Error fetching shows:', error);
     }
   }
+
+  calculerCentre(shows: Show[]): { lat: number; lng: number } {
+    let totalLat = 0;
+    let totalLng = 0;
+    const count = shows.length;
+
+    if (count === 0) {
+      return { lat: 0, lng: 0 }; // Default return if no shows
+    }
+
+    for (const show of shows) {
+      totalLat += parseFloat(show.lat);
+      totalLng += parseFloat(show.long);
+    }
+
+    return {
+      lat: totalLat / count, 
+      lng: totalLng / count  
+    };
+  }
+
+  toNumber(value: string): number {
+    return parseFloat(value);
+  }
+
 }
 
 
